2161c21a0d64341c33b1c90bd97ab0836ef4e09e
Amashiro Nutsuki <nutsukiamsr3@gmail.com>
2023-04-11 18:13:15 +0700

    Update BackupFile

--- src/AES/BackupFile.java
+++ src/AES/BackupFile.java
@@ -8,20 +8,25 @@ import java.nio.file.StandardCopyOption;
 public class BackupFile {
 
     public static void backup(File files) {
-        String filePath = files.toString();
-        File file = new File(filePath);
-        File backupFol = new File(file.getParent(), "Backup");
-        if (!backupFol.exists() || !backupFol.isDirectory()) {
-            backupFol.mkdir();
+        if (files.exists()) {
+            String filePath = files.toString();
+            File file = new File(filePath);
+            File backupFol = new File(file.getParent(), "Backup");
+            if (!backupFol.exists() || !backupFol.isDirectory()) {
+                backupFol.mkdir();
+            }
+            String fileName = file.getName();
+            String Extension = fileName;
+            Path source = file.toPath();
+            Path target = Paths.get(backupFol.getPath() + File.separator + Extension);
+            try {
+                Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-        String fileName = file.getName();
-        String Extension = fileName;
-        Path source = file.toPath();
-        Path target = Paths.get(backupFol.getPath() + File.separator + Extension);
-        try {
-            Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
-        } catch (IOException e) {
-            e.printStackTrace();
+        else {
+            System.out.println("\n\u001B[31mThe file could not be backed up because the file could not be found at >>> \u001B[0m" + files + "\n");
         }
     }
 }
